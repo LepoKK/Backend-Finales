9979b3eae1896e18e6b05fed16492d8df43292fd
LepoKK <lepo.kylm@tptlive.ee>
2024-10-21 17:39:08 +0300

    First Commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const nunjucks = require(`nunjucks`);
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({
+  extended:true
+}))
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+app.get('/', (req, res) => {
+  res.render('index.njk');
+  //console.log (`somebody visited`);
+});
+app.get(`/page2`, (req, res)=> {
+    res.render(`page2.njk`);
+});
+app.get(`/form`, (req, res)=> {
+  console.log(req.query)
+  res.render(`form.njk`, req.query);
+});
+app.get(`/circle`, (req, res)=> {
+  res.render(`circle.njk`)
+});
+
+app.post('/circle', (req, res) => {
+    const radius = parseFloat(req.body.radius);
+    const area = Math.PI * radius * radius;
+    const circumference = 2 * Math.PI * radius;
+    const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+  
+    res.render('circleAnswers.njk', {
+      r: radius,
+      a: area,
+      c: circumference,
+      v: volume
+    });
+  });
+
+const movieController = require('./src/movieController.js');
+app.use('/movies', movieController);
+
+const authController = require('./src/authController.js');
+app.use(authController);
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
